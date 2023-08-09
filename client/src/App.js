beb28c5cbfbc7e8e36a818800bd1a1d42bc579f8
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-09 21:57:44 +0300

    Client <> Added Part Of LoginFormm Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom"
-import HomePage from './Pages/HomePage';
+import LoginPage from "./Pages/LoginPage";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LoginPage/>} />
     </Routes> 
   );
 }
---
+++ client/src/Components/LoginForm/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css"
+import myImage from "../../assets/instagram.svg"
+ 
+const LoginForm = () => {
+    return (
+        <>
+          <div className="form-container">
+            <img src={myImage} alt="" />
+
+          </div>
+     
+       </>
+    );
+}
+export default LoginForm;
